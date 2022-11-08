a7be5c6135fa1d62f2ff0e440485581c904ce9bb
davidpanasuk <116981302+davidpanasuk@users.noreply.github.com>
2022-11-08 17:09:54 -0500

    Create Grocery.h

---
+++ Grocery.h
@@ -0,0 +1,29 @@
+/*
+ * Grocery.h
+ *
+ * Author: Eddy Sabbat (40134906) and David Panasuk (40223689)
+ *
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+#ifndef GROCERY_H_
+#define GROCERY_H_
+
+class Grocery{
+	public :
+	void print() const;
+	int getCost() const;
+	~Grocery();
+
+	private:
+
+
+
+};
+
+
+
+#endif /* GROCERY_H_ */
